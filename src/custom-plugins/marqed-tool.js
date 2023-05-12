f27e543c6213b37383e916d6904a39c406cc8a0b
Quentin Glorieux <quentin.glorieux@gmail.com>
2023-05-12 09:56:56 +0200

    update src/custom-plugins/index.css and marqed-tool.js

--- src/custom-plugins/marqed-tool.js
+++ src/custom-plugins/marqed-tool.js
@@ -1,4 +1,5 @@
 import { Directus } from '@directus/sdk';
+import './index.css';
 
 export default class MarqedTool {
 	static get isInline() {
@@ -18,13 +19,13 @@ export default class MarqedTool {
 		this.api = api;
 		this.button = null;
 		this._state = false;
-    this.data = {
-      url: window.location.pathname // Get the current URL
-    };
-
+		this.data = {
+			url: window.location.pathname, // Get the current URL
+		};
 
 		this.tag = 'MARK';
-		this.class = 'cdx-marker';
+		this.class = 'comment-marker';
+		this.class = 'comment-paragraph';
 	}
 
 	render() {
@@ -47,10 +48,15 @@ export default class MarqedTool {
 
 	wrap(range) {
 		const selectedText = range.extractContents();
+		const words = selectedText.textContent.trim().split(/\s+/);
+		const containsMultipleWords = words.length > 1;
+    console.log(containsMultipleWords)
+
 		const mark = document.createElement(this.tag);
+    this.class = words.length > 1 ? 'comment-paragraph' : 'comment-marker'
 
 		mark.classList.add(this.class);
-    mark.setAttribute('data-linkedcomment','');
+		mark.setAttribute('data-linkedcomment', '');
 		mark.appendChild(selectedText);
 		range.insertNode(mark);
 
@@ -69,6 +75,7 @@ export default class MarqedTool {
 	checkState() {
 		const mark = this.api.selection.findParentTag(this.tag);
 
+    // Double negation to convert to boolean
 		this.state = !!mark;
 
 		if (this.state) {
@@ -79,26 +86,25 @@ export default class MarqedTool {
 	}
 
 	renderActions() {
-
-    const parts = this.data.url.split("/");
-    const id = parts.pop(); 
-    console.log(id)
+		const parts = this.data.url.split('/');
+		const id = parts.pop();
+		console.log(id);
 
 		this.dropdown = document.createElement('select');
 
 		const directus = new Directus('http://localhost:8055');
 		directus
 			.items('sources')
-			.readOne(id,{
+			.readOne(id, {
 				fields: ['comments.id', 'comments.title'],
 			})
 			.then((response) => {
-        console.log(response.comments)
+				console.log(response.comments);
 
-        var option = document.createElement('option');
-        option.value = 'id';
-        option.text = '---';
-        this.dropdown.appendChild(option);
+				var option = document.createElement('option');
+				option.value = 'id';
+				option.text = '---';
+				this.dropdown.appendChild(option);
 				for (var i = 0; i < response.comments.length; i++) {
 					var option = document.createElement('option');
 					option.value = response.comments[i].id;
@@ -116,32 +122,26 @@ export default class MarqedTool {
 	showActions(mark) {
 		this.dropdown.onchange = () => {
 			if (this.dropdown.selectedIndex) {
-				// var selectedComment = this.dropdown.selectedIndex;
-        const selectedIndex = this.dropdown.selectedIndex;
-        const selectedOption = this.dropdown.options[selectedIndex];
-        const selectedValue = selectedOption.value;
-        mark.dataset.linkedcomment = selectedValue;
+				const selectedIndex = this.dropdown.selectedIndex;
+				const selectedOption = this.dropdown.options[selectedIndex];
+				const selectedValue = selectedOption.value;
+				mark.dataset.linkedcomment = selectedValue;
 			}
-      // if (selectedValue) {
-      //   console.log(selectedValue)
-      //   mark.dataset.linkedcomment = selectedValue;
-      // }
 		};
 		this.dropdown.hidden = false;
-		
 	}
 
 	hideActions() {
 		this.dropdown.onchange = null;
 		this.dropdown.hidden = true;
 	}
-	
+
 	static get sanitize() {
 		return {
 			mark: {
-                class: 'cdx-marker',
-                'data-linkedcomment': true
-            }
+				class: true,
+				'data-linkedcomment': true,
+			},
 		};
-		}
+	}
 }
